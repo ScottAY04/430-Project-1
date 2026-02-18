87cfbaa6dddec6d8c6e42f4d3e43c5c384c84470
Scott Au Yeung (RIT Student) <sa2975@rit.edu>
2026-02-18 12:46:05 -0500

    json response changed duplicated content-type

--- src/jsonResponse.js
+++ src/jsonResponse.js
@@ -7,7 +7,7 @@ const respondJSON = (request, response, status, object) => {
     const content = JSON.stringify(object);
     response.writeHead(status, {
         'Content-Type': 'application/json',
-        'Content-Type': Buffer.byteLength(content, 'utf8'),
+        'Content-Length': Buffer.byteLength(content, 'utf8'),
     });
     if(request.method !== 'HEAD' || status !== 204){
         response.write(content);
